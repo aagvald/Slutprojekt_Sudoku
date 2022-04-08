72f52a9b0ea9839dea355957a09abad089c89f01
aagvald <arvid.agvald@student.tabyenskilda.se>
2022-04-08 11:28:19 +0200

    första grid loop

--- sudoku_generator.py
+++ sudoku_generator.py
@@ -1,1 +1,18 @@
+import random
 
+grid = [[row, column] for row in range(9) for column in range(9)]
+
+print(grid)
+
+row_list = []
+column_list = []
+house_list = []
+
+row = []
+for i in range(9):
+    for i in grid:
+        if len(row) < 9:
+            row.append(i)
+    row_list.append(row)
+    row = []
+    del grid[0:9]
