2daa4e43bb42103d733b6b1c0a8da63e665c00ef
aagvald <arvid.agvald@student.tabyenskilda.se>
2022-04-16 16:12:11 +0200

    lite annan metod

--- sudoku_generator.py
+++ sudoku_generator.py
@@ -1,20 +1,46 @@
 import random
 
-grid = [[row, column] for row in range(9) for column in range(9)]
+#grid = [[row, column] for row in range(9) for column in range(9)]
 
-row_list = []
-column_list = []
-house_list = []
+#row_list = []
+#column_list = []
+#house_list = []
 
-row = []
-for i in range(9):
-    for i in grid:
-        if len(row) < 9:
-            row.append(i)
-    row_list.append(row)
-    row = []
-    del grid[0:9]
+#row = []
+#for i in range(9):
+    #for j in grid:
+        #if len(row) < 9:
+            #row.append(j)
+    #row_list.append(row)
+    #row = []
+    #del grid[0:9]
 
-for i in range(9):
-    for j in row_list:
-        column_list.append(j[i])
+#for i in range(9):
+    #for j in row_list:
+        #column_list.append(j[i])
+
+#house = []
+#for i in row_list:
+    #for j in range(3):
+        #if len(house) < 9:
+            #house.append(i[j])
+    #house_list.append(house)
+
+board = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+]
+
+for row in board:
+    for i in range(len(row)):
+        row[i] = random.randint(1,9)
+    print(row)
+
+print(board)
