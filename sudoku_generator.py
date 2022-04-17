e466948a634f8cc48ab6fbe34647f794678ac9c8
aagvald <arvid.agvald@student.tabyenskilda.se>
2022-04-17 14:59:46 +0200

    funktion

--- sudoku_generator.py
+++ sudoku_generator.py
@@ -17,34 +17,38 @@ for row in board:
         row[i] = random.randint(1,9)
     print(row)
 
-house_list = [[], [], [], [], [], [], [], [], []]
-for row in board:
+def create_house_list(board):
+    house_list = [[], [], [], [], [], [], [], [], []]
     
-    if board.index(row) < 3:  
-        for i in range(9):  
-            if i < 3:
-                house_list[0].append(row[i])
-            if 2 < i < 6:
-                house_list[1].append(row[i])
-            if 5 < i < 9:
-                house_list[2].append(row[i])
+    for row in board:
     
-    if 2 < board.index(row) < 6:
-        for i in range(9):  
-            if i < 3:
-                house_list[3].append(row[i])
-            if 2 < i < 6:
-                house_list[4].append(row[i])
-            if 5 < i < 9:
-                house_list[5].append(row[i])
+        if board.index(row) < 3:  
+            for i in range(9):  
+                if i < 3:
+                    house_list[0].append(row[i])
+                if 2 < i < 6:
+                    house_list[1].append(row[i])
+                if 5 < i < 9:
+                    house_list[2].append(row[i])
     
-    if 5 < board.index(row) < 9:
-        for i in range(9):  
-            if i < 3:
-                house_list[6].append(row[i])
-            if 2 < i < 6:
-                house_list[7].append(row[i])
-            if 5 < i < 9:
-                house_list[8].append(row[i])
-        
-print(house_list)
+        if 2 < board.index(row) < 6:
+            for i in range(9):  
+                if i < 3:
+                    house_list[3].append(row[i])
+                if 2 < i < 6:
+                    house_list[4].append(row[i])
+                if 5 < i < 9:
+                    house_list[5].append(row[i])
+    
+        if 5 < board.index(row) < 9:
+            for i in range(9):  
+                if i < 3:
+                    house_list[6].append(row[i])
+                if 2 < i < 6:
+                    house_list[7].append(row[i])
+                if 5 < i < 9:
+                    house_list[8].append(row[i])
+    
+    return house_list
+
+print(create_house_list(board))
