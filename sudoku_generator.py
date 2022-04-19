49b421e7620f3242b4f04ad58b92b97e7753dcb2
aagvald <arvid.agvald@student.tabyenskilda.se>
2022-04-19 18:22:35 +0200

    h

--- sudoku_generator.py
+++ sudoku_generator.py
@@ -15,22 +15,43 @@ board = [
 
 column_list = [[], [], [], [], [], [], [], [], []]
 
+#for row in board:
+    #for col in range(len(row)):
+        
+        #while True:
+            #random_num_candidate = random.randint(1, 9)
+
+            #if random_num_candidate not in row:
+                 #row[col] = random_num_candidate
+                 #column_list[col].append(row[col])
+                 #break
+
+    #print(row)
+
+
+
 for row in board:
+
+    numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
     for col in range(len(row)):
         
         while True:
-            random_num_candidate = random.randint(1, 9)
-
-            if random_num_candidate not in row:
-                 row[col] = random_num_candidate
-                 column_list[col].append(row[col])
-                 break
+            random_num_candidate = random.choice(numbers)
+            
+            if random_num_candidate not in column_list[col]:
+                row[col] = random_num_candidate
+                break
+            
+        column_list[col].append(row[col])
+        numbers.remove(random_num_candidate)
 
     print(row)
 
-print( )
 
-print(column_list)
+#print( )
+
+#print(column_list)
 
 
 #for row in range(len(board)):
@@ -43,12 +64,3 @@ print(column_list)
                 #print(True)
                 #print(board[row][col])
                 #print(row)
-            
-
-
-print( )
-
-for row in board:
-    print(row)
-
-#print(create_house_list(board))
