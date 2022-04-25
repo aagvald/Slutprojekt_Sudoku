0961d1c8c2a07e061268c3d32e8a0182d4a28ee7
aagvald <arvid.agvald@student.tabyenskilda.se>
2022-04-25 13:09:10 +0200

    h

--- house_list_generator.py
+++ house_list_generator.py
@@ -29,3 +29,47 @@ def create_house_list(board):
                     house_list[8].append(row[col])
     
     return house_list
+
+
+def is_house_unique(board, row):
+    for col in range(9):
+
+        if board.index(row) == 1 or board.index(row) == 4 or board.index(row) == 7:
+            row_above = board.index(row) - 1
+            for i in range(row_above, board.index(row)):
+                
+                if col < 3:
+                    for k in range(3):
+                        if board[row_above][k] == row[col]:
+                            return False
+            
+                if 2 < col < 6:
+                    for k in range(3, 6):
+                        if board[row_above][k] == row[col]:
+                            return False
+            
+                if 5 < col < 9:
+                    for k in range(6, 9):
+                        if board[row_above][k] == row[col]:
+                            return False
+                
+        if board.index(row) == 2 or board.index(row) == 5 or board.index(row) == 8:
+            row_above = board.index(row) - 2
+            for i in range(row_above, board.index(row)):
+
+                if col < 3:
+                    for k in range(3):
+                        if board[row_above][k] == row[col]:
+                            return False
+            
+                if 2 < col < 6:
+                    for k in range(3, 6):
+                        if board[row_above][k] == row[col]:
+                            return False
+            
+                if 5 < col < 9:
+                    for k in range(6, 9):
+                        if board[row_above][k] == row[col]:
+                            return False
+    
+    return True
