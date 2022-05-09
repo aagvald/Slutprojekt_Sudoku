fbe03a252b97e3d8d0677e0c9b343b466c76d8bc
aagvald <arvid.agvald@student.tabyenskilda.se>
2022-05-09 23:15:46 +0200

    h

--- scout_candidates.py
+++ scout_candidates.py
@@ -1,8 +1,25 @@
 from house_list_generator import create_house_list
-from candidate_check import determine_candidates
+from decide_candidates import determine_candidates
+
+numbers = [1,2,3,4,5,6,7,8,9]
 
 def house_list_candidates(board):
     candidate_list = determine_candidates(board)
-    house_list_candidates = create_house_list(candidate_list)
-    return house_list_candidates
+    house_cand_list = create_house_list(candidate_list)
+    return house_cand_list
+
+
+def find_single_cand_in_house(board):
+    house_cand_list = house_list_candidates(board)
+    for house in range(9):
+        for num in numbers:
+
+            digit_cand_count = 0
+            for cell in range(9):
+                if num in house_cand_list[house][cell]:
+                    digit_cand_count += 1
+
+            if digit_cand_count == 1:
+                house_cell_digit = [house, cell, num]
+                return house_cell_digit
 
