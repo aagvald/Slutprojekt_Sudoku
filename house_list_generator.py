5c31f7e6a73ac8a3c4ead0ceb529515494ac4fbe
aagvald <arvid.agvald@student.tabyenskilda.se>
2022-05-09 23:16:38 +0200

    h

--- house_list_generator.py
+++ house_list_generator.py
@@ -57,45 +57,53 @@ def identify_house_number(row,col):
             return 8
 
 
-def is_house_unique(board, row):
-    for col in range(9):
-
-        if board.index(row) == 1 or board.index(row) == 4 or board.index(row) == 7:
-            row_above = board.index(row) - 1
-            for i in range(row_above, board.index(row)):
-                
-                if col < 3:
-                    for k in range(3):
-                        if board[row_above][k] == row[col]:
-                            return False
-            
-                if 2 < col < 6:
-                    for k in range(3, 6):
-                        if board[row_above][k] == row[col]:
-                            return False
-            
-                if 5 < col < 9:
-                    for k in range(6, 9):
-                        if board[row_above][k] == row[col]:
-                            return False
-                
-        if board.index(row) == 2 or board.index(row) == 5 or board.index(row) == 8:
-            row_above = board.index(row) - 2
-            for i in range(row_above, board.index(row)):
-
-                if col < 3:
-                    for k in range(3):
-                        if board[row_above][k] == row[col]:
-                            return False
-            
-                if 2 < col < 6:
-                    for k in range(3, 6):
-                        if board[row_above][k] == row[col]:
-                            return False
-            
-                if 5 < col < 9:
-                    for k in range(6, 9):
-                        if board[row_above][k] == row[col]:
-                            return False
+def identify_row(house_number,cell_number):
+    if house_number < 3:
+        if cell_number < 3:
+            return 0
+        if 2 < cell_number < 6:
+            return 1
+        if 5 < cell_number < 9:
+            return 2
     
-    return True
+    if 2 < house_number < 6:
+        if cell_number < 3:
+            return 3
+        if 2 < cell_number < 6:
+            return 4
+        if 5 < cell_number < 9:
+            return 5
+    
+    if 5 < house_number < 9:
+        if cell_number < 3:
+            return 6
+        if 2 < cell_number < 6:
+            return 7
+        if 5 < cell_number < 9:
+            return 8
+
+
+def identify_col(house_number,cell_number):
+    if house_number == 0 or house_number == 3 or house_number == 6:
+        if cell_number < 3:
+            return cell_number
+        if 2 < cell_number < 6:
+            return cell_number - 3
+        if 5 < cell_number < 9:
+            return cell_number - 6
+    
+    if house_number == 1 or house_number == 4 or house_number == 7:
+        if cell_number < 3:
+            return cell_number + 3
+        if 2 < cell_number < 6:
+            return cell_number
+        if 5 < cell_number < 9:
+            return cell_number - 3
+        
+    if house_number == 2 or house_number == 5 or house_number == 8:
+        if cell_number < 3:
+            return cell_number + 6
+        if 2 < cell_number < 6:
+            return cell_number + 3
+        if 5 < cell_number < 9:
+            return cell_number
--- sudoko_solver.py
+++ sudoko_solver.py
@@ -1,6 +1,7 @@
 import random
-from candidate_check import determine_candidates
-from scout_candidates import house_list_candidates
+from decide_candidates import determine_candidates
+from scout_candidates import house_list_candidates, find_single_cand_in_house
+from house_list_generator import identify_row, identify_col
 
 board = [
     [5,0,0,0,0,0,3,4,0],
@@ -17,5 +18,6 @@ board = [
 for row in board:
     print(row)
 
-print(determine_candidates(board))
-print("\n" + str(house_list_candidates(board)))
+cell_info = find_single_cand_in_house(board)
+single_cand_row = (identify_row(cell_info[0],cell_info[1]))
+single_cand_col = (identify_col(cell_info[0],cell_info[1]))
