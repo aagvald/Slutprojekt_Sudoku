ee4724ec4b1e588e133102f535902a6775b6f1e8
aagvald <arvid.agvald@student.tabyenskilda.se>
2022-05-14 18:19:32 +0200

    h

--- fill_in_cell.py
+++ fill_in_cell.py
@@ -9,3 +9,37 @@ def fill_single_cand_in_house(board):
         board[row][col] = num
 
 
+def fill_single_cand_in_row(board):
+    while find_single_cand_in_row(board) != False:
+        row_col_num = find_single_cand_in_row(board)
+        row = row_col_num[0]
+        col = row_col_num[1]
+        num = row_col_num[2]
+        board[row][col] = num
+
+
+def fill_single_cand_in_col(board):
+    while find_single_cand_in_col(board) != False:
+        row_col_num = find_single_cand_in_col(board)
+        row = row_col_num[0]
+        col = row_col_num[1]
+        num = row_col_num[2]
+        board[row][col] = num
+
+
+def fill_board(board):
+    x = False
+    while x == False:
+        fill_single_cand_in_house
+        fill_single_cand_in_row
+        fill_single_cand_in_col
+
+        empty_cells = 0
+        for row in range(9):
+            for col in range(9):
+                if board[row][col] == 0:
+                    empty_cells += 1
+        
+        if empty_cells == 0:
+            x = True
+    return True
--- sudoko_solver.py
+++ sudoko_solver.py
@@ -2,7 +2,7 @@ import random
 from decide_candidates import determine_candidates
 from single_candidates import house_list_candidates, find_single_cand_in_house, find_single_cand_in_row, find_single_cand_in_col
 from house_list_generator import identify_row, identify_col
-from fill_in_cell import fill_single_cand_in_house
+from fill_in_cell import fill_single_cand_in_house, fill_single_cand_in_row, fill_single_cand_in_col, fill_board
 
 board = [
     [5,0,0,0,0,0,3,4,0],
@@ -20,8 +20,25 @@ for row in board:
     print(row)
 
 print("")
-fill_single_cand_in_house(board)
+board = [
+    [5, 9, 7, 2, 6, 8, 3, 4, 1],
+    [2, 8, 1, 4, 3, 7, 5, 0, 0],
+    [4, 3, 6, 9, 1, 5, 2, 7, 8],
+    [0, 6, 2, 3, 8, 4, 1, 5, 7],
+    [0, 1, 4, 7, 5, 0, 0, 2, 0],
+    [0, 7, 5, 1, 2, 0, 0, 0, 4],
+    [7, 5, 0, 8, 9, 1, 4, 0, 2],
+    [6, 4, 0, 5, 7, 2, 0, 1, 0],
+    [1, 2, 0, 6, 4, 3, 7, 0, 5],
+]
 
 for row in board:
     print(row)
 
+print(fill_single_cand_in_house(board))
+#print(house_list_candidates(board)[8][1])
+#print(find_single_cand_in_house(board))
+#fill_single_cand_in_row(board)
+
+for row in board:
+    print(row)
