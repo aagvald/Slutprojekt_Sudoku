7521891f96d92e619387c4f17425dc21a4b57f5e
aagvald <arvid.agvald@student.tabyenskilda.se>
2022-05-14 12:16:39 +0200

    h

--- single_candidates.py
+++ single_candidates.py
@@ -14,13 +14,13 @@ def find_single_cand_in_house(board):
     for house in range(9):
         for num in numbers:
 
-            digit_cand_count = 0
+            digit_cand_count = []
             for cell in range(9):
                 if num in house_cand_list[house][cell]:
-                    digit_cand_count += 1
+                    digit_cand_count.append([house, cell, num])
 
-            if digit_cand_count == 1:
-                house_cell_digit = [house, cell, num]
+            if len(digit_cand_count) == 1:
+                house_cell_digit = digit_cand_count[0]
                 row = identify_row(house_cell_digit[0],house_cell_digit[1])
                 col = identify_col(house_cell_digit[0],house_cell_digit[1])
                 row_col_digit = [row, col, num]
@@ -32,13 +32,13 @@ def find_single_cand_in_row(board):
     for row in range(9):
         for num in numbers:
 
-            digit_cand_count = 0
+            digit_cand_count = []
             for col in range(9):
                 if num in candidate_list[row][col]:
-                    digit_cand_count += 1
+                    digit_cand_count.append([row, col, num])
             
-            if digit_cand_count == 1:
-                row_col_digit = [row, col, num]
+            if len(digit_cand_count) == 1:
+                row_col_digit = digit_cand_count[0]
                 return row_col_digit
 
 
@@ -47,11 +47,11 @@ def find_single_cand_in_col(board):
     for col in range(9):
         for num in numbers:
 
-            digit_cand_count = 0
+            digit_cand_count = []
             for row in range(9):
                 if num in candidate_list[row][col]:
-                    digit_cand_count += 1
+                    digit_cand_count.append([row, col, num])
             
-            if digit_cand_count == 2:
-                row_col_digit = [row, col, num]
+            if len(digit_cand_count) == 1:
+                row_col_digit = digit_cand_count[0]
                 return row_col_digit
