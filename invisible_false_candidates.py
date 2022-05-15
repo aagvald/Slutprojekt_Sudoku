9cd971313d83e078dd55bcc0db622bfda134f457
aagvald <arvid.agvald@student.tabyenskilda.se>
2022-05-15 16:00:13 +0200

    h

--- invisible_false_candidates.py
+++ invisible_false_candidates.py
@@ -1,8 +1,9 @@
-from house_list_generator import create_house_list, identify_house_number, identify_row, identify_col
+import random
+from turtle import color
+from house_list_generator import create_house_list, identify_house_number, identify_cell_in_house, identify_row, identify_col
 from decide_candidates import determine_candidates, house_list_candidates
 
 numbers = [1,2,3,4,5,6,7,8,9]
 
-#def find_invis_false_cand(board):
-    #for num in numbers:
-        #for col in range(9):
+#def brute_force(board):
+    #for row in board:
