00656f532fa3d56e0608c5fe08287d41d42079e4
aagvald <arvid.agvald@student.tabyenskilda.se>
2022-05-15 03:20:53 +0200

    h

--- solve_board.py
+++ solve_board.py
@@ -1,30 +1,39 @@
 from single_candidates import find_single_cand_in_house, find_single_cand_in_row, find_single_cand_in_col
 
 def fill_single_cand_in_house(board):
-    row_col_num = find_single_cand_in_house(board)
-    if row_col_num != False:
-        row = row_col_num[0]
-        col = row_col_num[1]
-        num = row_col_num[2]
-        board[row][col] = num
+    while True:
+        row_col_num = find_single_cand_in_house(board)
+        if row_col_num == False:
+            break
+        else:
+            row = row_col_num[0]
+            col = row_col_num[1]
+            num = row_col_num[2]
+            board[row][col] = num
 
 
 def fill_single_cand_in_row(board):
-    row_col_num = find_single_cand_in_row(board)
-    if row_col_num != False:
-        row = row_col_num[0]
-        col = row_col_num[1]
-        num = row_col_num[2]
-        board[row][col] = num
+    while True:
+        row_col_num = find_single_cand_in_row(board)
+        if row_col_num == False:
+            break
+        else:
+            row = row_col_num[0]
+            col = row_col_num[1]
+            num = row_col_num[2]
+            board[row][col] = num
 
 
 def fill_single_cand_in_col(board):
-    row_col_num = find_single_cand_in_col(board)
-    if row_col_num != False:
-        row = row_col_num[0]
-        col = row_col_num[1]
-        num = row_col_num[2]
-        board[row][col] = num
+    while True:
+        row_col_num = find_single_cand_in_col(board)
+        if row_col_num == False:
+            break
+        else:
+            row = row_col_num[0]
+            col = row_col_num[1]
+            num = row_col_num[2]
+            board[row][col] = num
 
 
 def is_board_complete(board):
@@ -37,6 +46,3 @@ def is_board_complete(board):
         return True
     else:
         return False
-
-
-#def fill_board(board):
--- sudoko_solver.py
+++ sudoko_solver.py
@@ -3,7 +3,7 @@ from termios import FIOASYNC
 from decide_candidates import determine_candidates
 from single_candidates import house_list_candidates, find_single_cand_in_house, find_single_cand_in_row, find_single_cand_in_col
 from house_list_generator import identify_row, identify_col
-from solve_board import fill_single_cand_in_house, fill_single_cand_in_row, fill_single_cand_in_col
+from solve_board import fill_single_cand_in_house, fill_single_cand_in_row, fill_single_cand_in_col, is_board_complete, fill_single_cand_in_house2
 
 board = [
     [5,0,0,0,0,0,3,4,0],
@@ -37,17 +37,8 @@ for row in board:
     print(row)
 print("")
 
-for i in range(8):
-    fill_single_cand_in_house(board)
-#print(house_list_candidates(board)[8][1])
-#print(find_single_cand_in_house(board))
-#fill_single_cand_in_row(board)
 
-#for row in board:
-    #print(row)
-
-print(house_list_candidates(board))
-#print(find_single_cand_in_house(board))
+fill_single_cand_in_house2(board)
 
 print("")
 for row in board:
